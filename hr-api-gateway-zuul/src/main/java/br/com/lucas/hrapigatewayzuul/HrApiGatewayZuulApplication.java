925f12efe2fd2cbceb66978fcc0761fed5bb6125
Lucas Oliveira <lcsd.lucas@gmail.com>
2021-03-24 22:30:03 -0300

    removendo annotation

--- hr-api-gateway-zuul/src/main/java/br/com/lucas/hrapigatewayzuul/HrApiGatewayZuulApplication.java
+++ hr-api-gateway-zuul/src/main/java/br/com/lucas/hrapigatewayzuul/HrApiGatewayZuulApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableZuulProxy
-@EnableDiscoveryClient
 public class HrApiGatewayZuulApplication {
 //    @Bean
 //    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
